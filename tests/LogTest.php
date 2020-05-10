88b905e8674d3c932905ed8db68f1a3db26f3bff
kanto <watanabe.kanto@gmail.com>
2020-05-10 13:24:48 +0900

    add: LogTest.php

--- tests/LogTest.php
+++ tests/LogTest.php
@@ -7,7 +7,25 @@ class LogTest extends TestCase
 {
     public function testInit()
     {
+        $class = new ReflectionClass('Kore\\Log');
+        $property = $class->getProperty('logName');
+        $property->setAccessible(true);
+
+        $this->assertSame('app', $property->getValue());
+
         Log::init('test');
-        
+        $this->assertSame('test', $property->getValue());
     }
+
+    public function testDebug()
+    {  
+        Log::init('test');
+        $logfile = LOGS_DIR.'test-'.date("Y-m-d").'.log';
+        @unlink($logfile);
+
+        Log::debug('test!');
+        $this->assertFileExists($logfile);
+    }
+
+
 }
