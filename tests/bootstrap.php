50a36c26c5129333a1d1c45259c872b507fbd0a8
kanto <watanabe.kanto@gmail.com>
2020-08-17 01:12:03 +0900

    Refactor test code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,18 +8,7 @@ define('ROOT_DIR', __DIR__.'/');
 define('APP_DIR', ROOT_DIR.'mock/');
 define('APP_NS', 'mock\\');
 
-// define directory
-define('CONFIG_DIR', APP_DIR.'config/');
-define('CONTROLLERS_DIR', APP_DIR.'controllers/');
-define('COMMANDS_DIR', APP_DIR.'commands/');
-define('VIEWS_DIR', APP_DIR.'views/');
-define('LIBS_DIR', APP_DIR.'libs/');
-define('TMP_DIR', APP_DIR.'tmp/');
-define('LOGS_DIR', TMP_DIR.'logs/');
-
-// define namespace
-define('CONTROLLERS_NS', APP_NS.'controllers\\');
-define('COMMANDS_NS', APP_NS.'commands\\');
+require(__DIR__.'/../src/bootstrap.php');
 
 // AutoLoad
 spl_autoload_register(function ($class) {
