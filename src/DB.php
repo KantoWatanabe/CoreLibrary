1af5d321b17e7f9add8a0b424644c9d36ba62df3
kanto <watanabe.kanto@gmail.com>
2020-11-22 22:55:23 +0900

    [Upgrade] Upgrade PHPDoc in DB.php

--- src/DB.php
+++ src/DB.php
@@ -1,18 +1,32 @@
 <?php
+/**
+ * Kore : Simple And Minimal Framework
+ *
+ */
+
 namespace Kore;
 
 use Kore\Config;
 use Kore\Log;
 
+/**
+ * DB class
+ *
+ * Managing database operations
+ */
 class DB
 {
     /**
+     * \PDO instance
+     *
      * @var \PDO
      */
     protected $pdo;
 
     /**
-     * @param string $dbconfig
+     * __construct method
+     *
+     * @param string $dbconfig DB configuration key
      * @return void
      */
     final private function __construct($dbconfig)
@@ -21,8 +35,10 @@ class DB
     }
 
     /**
+     * __clone method
+     *
      * @return void
-     * @throws \Exception
+     * @throws \Exception Thrown in when __clone is called
      */
     final public function __clone()
     {
@@ -30,8 +46,10 @@ class DB
     }
 
     /**
-     * @param string $dbconfig
-     * @return self
+     * Get DB instance corresponding to the DB configuration key
+     *
+     * @param string $dbconfig DB configuration key
+     * @return self DB instance
      */
     public static function connection($dbconfig='database')
     {
@@ -43,9 +61,12 @@ class DB
     }
 
     /**
-     * @param string $dbconfig
+     * Access DB
+     *
+     * @param string $dbconfig DB configuration key
      * @return void
-     * @throws \PDOException
+     * @throws \Exception Thrown in when the DB configuration does not exist
+     * @throws \PDOException Thrown when DB access fails
      */
     protected function connect($dbconfig)
     {
@@ -66,7 +87,9 @@ class DB
     }
 
     /**
-     * @return \PDO
+     * Get \PDO instance
+     *
+     * @return \PDO \PDO instance
      */
     public function getPdo()
     {
@@ -74,9 +97,11 @@ class DB
     }
 
     /**
-     * @param string $query
-     * @param array<mixed> $params
-     * @return array<mixed>
+     * Run the select statement
+     *
+     * @param string $query SQL query
+     * @param array<mixed> $params SQL query parameters
+     * @return array<mixed> query results
      */
     public function select($query, $params = [])
     {
@@ -87,9 +112,11 @@ class DB
     }
 
     /**
-     * @param string $query
-     * @param array<mixed> $params
-     * @return array<mixed>
+     * Run the select statement, get only one record
+     *
+     * @param string $query SQL query
+     * @param array<mixed> $params SQL query parameters
+     * @return array<mixed> query results
      */
     public function selectFirst($query, $params = [])
     {
@@ -100,9 +127,11 @@ class DB
     }
 
     /**
-     * @param string $query
-     * @param array<mixed> $params
-     * @return mixed
+     * Run the select count statement
+     *
+     * @param string $query SQL query
+     * @param array<mixed> $params SQL query parameters
+     * @return mixed query results
      */
     public function count($query, $params = [])
     {
@@ -113,9 +142,11 @@ class DB
     }
 
     /**
-     * @param string $query
-     * @param array<mixed> $params
-     * @return string
+     * Run the insert statement
+     *
+     * @param string $query SQL query
+     * @param array<mixed> $params SQL query parameters
+     * @return string last insert ID
      */
     public function insert($query, $params = [])
     {
@@ -125,9 +156,11 @@ class DB
     }
 
     /**
-     * @param string $query
-     * @param array<mixed> $params
-     * @return int
+     * Run the update statement
+     *
+     * @param string $query SQL query
+     * @param array<mixed> $params SQL query parameters
+     * @return int number of updated records
      */
     public function update($query, $params = [])
     {
@@ -137,9 +170,11 @@ class DB
     }
 
     /**
-     * @param string $query
-     * @param array<mixed> $params
-     * @return int
+     * Run the delete statement
+     *
+     * @param string $query SQL query
+     * @param array<mixed> $params SQL query parameters
+     * @return int number of deleted records
      */
     public function delete($query, $params = [])
     {
@@ -149,7 +184,10 @@ class DB
     }
 
     /**
-     * @param callable $callback
+     * Run in a transaction
+     *
+     * Rollback if an exception is raised in the callback.
+     * @param callable $callback callback processing
      * @return void
      */
     public function transaction($callback)
@@ -165,9 +203,19 @@ class DB
     }
 
     /**
-     * @param string $marker
-     * @param array<mixed> $values
-     * @return array<mixed>
+     * Get the IN clause
+     *
+     * @param string $marker parameter marker
+     * @param array<mixed> $values value specified in the IN clause
+     * @return array<mixed> IN clause information
+     *                      array(
+     *                          'IN (:MARKER_0, :MARKER_1, :MARKER_2)',
+     *                          array(
+     *                              'MARKER_0' => 'value',
+     *                              'MARKER_1' => 'value',
+     *                              'MARKER_2' => 'value'
+     *                          )
+     *                      )
      */
     public static function getInClause($marker, $values)
     {
@@ -186,9 +234,11 @@ class DB
     }
 
     /**
-     * @param string $query
-     * @param array<mixed> $params
-     * @return \PDOStatement<mixed>
+     * Run the query statement
+     *
+     * @param string $query SQL query
+     * @param array<mixed> $params SQL query parameters
+     * @return \PDOStatement<mixed> \PDOStatement object
      */
     private function execute($query, $params)
     {
@@ -212,8 +262,10 @@ class DB
     }
 
     /**
-     * @param mixed $value
-     * @return mixed
+     * Get the debug value from the variable
+     *
+     * @param mixed $value variable
+     * @return mixed debug value
      */
     private function debugValue($value)
     {
@@ -235,8 +287,10 @@ class DB
     }
 
     /**
-     * @param mixed $value
-     * @return int
+     * Get the data_type from the variable
+     *
+     * @param mixed $value variable
+     * @return int data_type
      */
     private function dataType($value)
     {
